8f6959f2950c79c9a010ced82c640f93c2dd2c4b
Sergey Klyusov <donot@email.me>
2019-12-18 20:14:20 +0500

    auto backup

--- gomapper_aux.go
+++ gomapper_aux.go
@@ -1,6 +1,20 @@
 package gomapper_aux
 
 import (
-  "fmt"
+  "strconv"
+  "strings"
+  "errors"
 )
 
+func LastResultDecode(s string) (string, int64, int64, string, error) {
+  a:= strings.Split(s, ":")
+  if len(a) < 4 { return "", 0, 0, "", errors.New("bad last_result format") }
+
+  i1, err := strconv.ParseInt(a[1], 10, 64)
+  if err != nil { return "", 0, 0, "", errors.New("bad last_result format") }
+
+  i2, err := strconv.ParseInt(a[2], 10, 64)
+  if err != nil { return "", 0, 0, "", errors.New("bad last_result format") }
+
+  return a[0], i1, i2, strings.Join(a[3:], ":"), nil
+}
