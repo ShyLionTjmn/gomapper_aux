7bfb8139a8ebb8a848fec171f83364b55210e41e
Sergey Klyusov <donot@email.me>
2019-12-26 20:14:19 +0500

    auto backup

--- rule.go
+++ rule.go
@@ -16,6 +16,8 @@ var graphIntKey_regex *regexp.Regexp
 var graphDevNeKey_regex *regexp.Regexp
 var graphIntNeKey_regex *regexp.Regexp
 
+var alertKey_regex *regexp.Regexp
+
 func init() {
   w.WhereAmI()
   fmt.Sprint()
@@ -23,6 +25,8 @@ func init() {
   graphIntKey_regex = regexp.MustCompile(`^int\.([0-9a-zA-Z_]+)[ \t]*(==|=~|!=|!~)[ \t]*([^\s])`)
   graphDevNeKey_regex = regexp.MustCompile(`^not_empty +dev\.([0-9a-zA-Z_]+)\s*($|[^\s])`)
   graphIntNeKey_regex = regexp.MustCompile(`^not_empty +int\.([0-9a-zA-Z_]+)\s*($|[^\s])`)
+
+  alertKey_regex = regexp.MustCompile(`^([0-9a-zA-Z_]+)[ \t]*(==|=~|!=|!~)[ \t]*([^\s])`)
 }
 
 
@@ -493,3 +497,321 @@ func resolveStack(a []interface{}) (bool, error) {
 //fmt.Println(b, "=", a[0])
   return a[0].(bool), nil
 }
+
+func ParseAlertRule(s string) ([]string, error) {
+  s_pos := 0
+  ret_s := make([]string, 0)
+
+  par_open := 0
+  and_or_started := false
+
+L1:for s_pos < len(s) {
+    for s_pos < len(s) && (s[s_pos] == ' ' || s[s_pos] == '\n' || s[s_pos] == '\t') { s_pos++ }
+    if s_pos == len(s) {
+      if par_open != 0 {
+        return nil, errors.New("No closing parenthesis")
+      }
+      if and_or_started {
+        return nil, errors.New("No next expression after logical operator")
+      }
+      return ret_s, nil
+    }
+    and_or_started = false
+    op := ""
+    if m := alertKey_regex.FindStringSubmatchIndex(s[s_pos:]); m != nil {
+      key := s[s_pos+m[2]:s_pos+m[3]]
+      if IndexOf(ret_s, key) < 0 {
+        ret_s = append(ret_s, key)
+      }
+      op = s[s_pos+m[4]:s_pos+m[5]]
+      s_pos += m[6] //at least 1 symbol left in string
+    } else if s[s_pos] == '(' {
+      par_open++
+      s_pos++
+      continue L1
+    } else {
+      return nil, errors.New("Syntax error: unexpected expression at "+strconv.Itoa(s_pos))
+    }
+
+    if op == "==" || op == "!=" {
+      var str_start int
+      var str_stop int
+      _ = str_start
+      _ = str_stop
+      if s[s_pos] != '"' {
+        str_start = s_pos
+        for s_pos < len(s) && s[s_pos] != ' ' && s[s_pos] != '\t' && s[s_pos] != '\n' { s_pos++ }
+        str_stop = s_pos
+      } else {
+        quote_closed := false
+        s_pos++
+        str_start = s_pos
+        for s_pos < len(s) {
+          if s[s_pos] == '\\' && (s_pos+1) < len(s) && (s[s_pos+1] == '\\' || s[s_pos+1] == '"') {
+            s_pos += 2
+          } else if s[s_pos] == '"' {
+            str_stop = s_pos
+            s_pos++
+            quote_closed = true
+            if s_pos < len(s) && s[s_pos] != '\n' && s[s_pos] != ' ' && s[s_pos] != '\t' {
+              return nil, errors.New("Syntax error: trailing symbols after quote at "+strconv.Itoa(s_pos))
+            }
+            break
+          } else if s[s_pos] == '\n' {
+            return nil, errors.New("Syntax error: unclosed quote on newline at "+strconv.Itoa(s_pos))
+          } else {
+            s_pos++
+          }
+        }
+        if !quote_closed {
+          return nil, errors.New("Syntax error: unclosed quote at "+strconv.Itoa(s_pos))
+        }
+      }
+    } else if op == "=~" || op == "!~" {
+      if s[s_pos] != '/' {
+        return nil, errors.New("Syntax error: no regex opening symbol \"/\" at "+strconv.Itoa(s_pos))
+      }
+      s_pos++
+      regex_start := s_pos
+      regex_closed := false
+      regex_pattern := ""
+      for s_pos < len(s) {
+        if s[s_pos] == '\\' && (s_pos+1) < len(s) && (s[s_pos+1] == '\\' || s[s_pos+1] == '/') {
+          regex_pattern += string(s[s_pos+1])
+          s_pos += 2
+        } else if s[s_pos] == '/' {
+          s_pos++
+          regex_closed = true
+          if s_pos < len(s) && s[s_pos] != '\n' && s[s_pos] != ' ' && s[s_pos] != '\t' {
+            return nil, errors.New("Syntax error: trailing symbols after regex end at "+strconv.Itoa(s_pos))
+          }
+          break
+        } else if s[s_pos] == '\n' {
+          return nil, errors.New("Syntax error: unclosed regex at "+strconv.Itoa(s_pos))
+        } else {
+          regex_pattern += string(s[s_pos])
+          s_pos++
+        }
+      }
+      if !regex_closed {
+        return nil, errors.New("Syntax error: no regex closing at "+strconv.Itoa(s_pos))
+      }
+      if _, err := regexp.Compile(regex_pattern); err != nil {
+        return nil, errors.New("Syntax error: regex compile error at "+strconv.Itoa(regex_start))
+      }
+    }
+
+
+    for s_pos < len(s) && (s[s_pos] == ' ' || s[s_pos] == '\n' || s[s_pos] == '\t') { s_pos++ }
+    if s_pos == len(s) {
+      if par_open != 0 {
+        return nil, errors.New("No closing parenthesis")
+      }
+      if and_or_started {
+        return nil, errors.New("No next expression after logical operator")
+      }
+      return ret_s, nil
+    }
+
+    for s_pos < len(s) && s[s_pos] == ')' {
+      if par_open == 0 {
+        return nil, errors.New("No opening parenthesis for ) at "+strconv.Itoa(s_pos))
+      }
+      if and_or_started {
+        return nil, errors.New("Syntax error: unexpected ) at "+strconv.Itoa(s_pos))
+      }
+      par_open--
+      s_pos ++
+      for s_pos < len(s) && (s[s_pos] == ' ' || s[s_pos] == '\n' || s[s_pos] == '\t') { s_pos++ }
+    }
+
+
+    if s_pos == len(s) {
+      if par_open != 0 {
+        return nil, errors.New("No closing parenthesis")
+      }
+      if and_or_started {
+        return nil, errors.New("No next expression after logical operator")
+      }
+      return ret_s, nil
+    } else if strings.Index(s[s_pos:], "||") == 0 || strings.Index(s[s_pos:], "&&") == 0 {
+      if and_or_started {
+        return nil, errors.New("Syntax error: unexpected logical operator at "+strconv.Itoa(s_pos))
+      } else {
+        and_or_started = true
+        s_pos += 2
+      }
+    } else {
+      return nil, errors.New("Syntax error: unexpected expression at "+strconv.Itoa(s_pos))
+    }
+
+  }
+  return ret_s, nil
+}
+
+func MatchAlertRule(s string, alert M) (bool, error) {
+  s_pos := 0
+  par_open := 0
+  and_or_started := false
+
+  stack := make([]interface{}, 0)
+
+L1:for s_pos < len(s) {
+    for s_pos < len(s) && (s[s_pos] == ' ' || s[s_pos] == '\n' || s[s_pos] == '\t') { s_pos++ }
+    if s_pos == len(s) {
+      if par_open != 0 {
+        return false, errors.New("No closing parenthesis")
+      }
+      if and_or_started {
+        return false, errors.New("No next expression after logical operator")
+      }
+      return resolveStack(stack)
+    }
+    and_or_started = false
+    op := ""
+    key := ""
+    cmp_value := ""
+
+
+    if m := alertKey_regex.FindStringSubmatchIndex(s[s_pos:]); m != nil {
+      key = s[s_pos+m[2]:s_pos+m[3]]
+      op = s[s_pos+m[4]:s_pos+m[5]]
+      s_pos += m[6] //at least 1 symbol left in string
+
+      cmp_value, _ = alert.Vse(key)
+    } else if s[s_pos] == '(' {
+      stack = append(stack, "(")
+      par_open++
+      s_pos++
+      continue L1
+    } else {
+      return false, errors.New("Syntax error: unexpected expression at "+strconv.Itoa(s_pos))
+    }
+
+    if op == "==" || op == "!=" {
+      var str_start int
+      var str_stop int
+      if s[s_pos] != '"' {
+        str_start = s_pos
+        for s_pos < len(s) && s[s_pos] != ' ' && s[s_pos] != '\t' && s[s_pos] != '\n' { s_pos++ }
+        str_stop = s_pos
+      } else {
+        quote_closed := false
+        s_pos++
+        str_start = s_pos
+        for s_pos < len(s) {
+          if s[s_pos] == '\\' && (s_pos+1) < len(s) && (s[s_pos+1] == '\\' || s[s_pos+1] == '"') {
+            s_pos += 2
+          } else if s[s_pos] == '"' {
+            str_stop = s_pos
+            s_pos++
+            quote_closed = true
+            if s_pos < len(s) && s[s_pos] != '\n' && s[s_pos] != ' ' && s[s_pos] != '\t' {
+              return false, errors.New("Syntax error: trailing symbols after quote at "+strconv.Itoa(s_pos))
+            }
+            break
+          } else if s[s_pos] == '\n' {
+            return false, errors.New("Syntax error: unclosed quote on newline at "+strconv.Itoa(s_pos))
+          } else {
+            s_pos++
+          }
+        }
+        if !quote_closed {
+          return false, errors.New("Syntax error: unclosed quote at "+strconv.Itoa(s_pos))
+        }
+      }
+
+      if op == "==" {
+        stack = append(stack, s[str_start:str_stop] == cmp_value)
+      } else {
+        stack = append(stack, s[str_start:str_stop] != cmp_value)
+      }
+    } else if op == "=~" || op == "!~" {
+      if s[s_pos] != '/' {
+        return false, errors.New("Syntax error: no regex opening symbol \"/\" at "+strconv.Itoa(s_pos))
+      }
+      s_pos++
+      regex_start := s_pos
+      regex_closed := false
+      regex_pattern := ""
+      for s_pos < len(s) {
+        if s[s_pos] == '\\' && (s_pos+1) < len(s) && (s[s_pos+1] == '\\' || s[s_pos+1] == '/') {
+          regex_pattern += string(s[s_pos+1])
+          s_pos += 2
+        } else if s[s_pos] == '/' {
+          s_pos++
+          regex_closed = true
+          if s_pos < len(s) && s[s_pos] != '\n' && s[s_pos] != ' ' && s[s_pos] != '\t' {
+            return false, errors.New("Syntax error: trailing symbols after regex end at "+strconv.Itoa(s_pos))
+          }
+          break
+        } else if s[s_pos] == '\n' {
+          return false, errors.New("Syntax error: unclosed regex at "+strconv.Itoa(s_pos))
+        } else {
+          regex_pattern += string(s[s_pos])
+          s_pos++
+        }
+      }
+      if !regex_closed {
+        return false, errors.New("Syntax error: no regex closing at "+strconv.Itoa(s_pos))
+      }
+      if reg, err := regexp.Compile("(?i:"+regex_pattern+")"); err != nil {
+        return false, errors.New("Syntax error: regex compile error at "+strconv.Itoa(regex_start))
+      } else {
+        if op == "=~" {
+          stack = append(stack, reg.MatchString(cmp_value))
+        } else {
+          stack = append(stack, !reg.MatchString(cmp_value))
+        }
+      }
+    }
+
+    for s_pos < len(s) && (s[s_pos] == ' ' || s[s_pos] == '\n' || s[s_pos] == '\t') { s_pos++ }
+    if s_pos == len(s) {
+      if par_open != 0 {
+        return false, errors.New("No closing parenthesis")
+      }
+      if and_or_started {
+        return false, errors.New("No next expression after logical operator")
+      }
+      return resolveStack(stack)
+    }
+
+    for s_pos < len(s) && s[s_pos] == ')' {
+      if par_open == 0 {
+        return false, errors.New("No opening parenthesis for ) at "+strconv.Itoa(s_pos))
+      }
+      if and_or_started {
+        return false, errors.New("Syntax error: unexpected ) at "+strconv.Itoa(s_pos))
+      }
+      par_open--
+      s_pos ++
+      stack = append(stack, ")")
+      for s_pos < len(s) && (s[s_pos] == ' ' || s[s_pos] == '\n' || s[s_pos] == '\t') { s_pos++ }
+    }
+
+    if s_pos == len(s) {
+      if par_open != 0 {
+        return false, errors.New("No closing parenthesis")
+      }
+      if and_or_started {
+        return false, errors.New("No next expression after logical operator")
+      }
+      return resolveStack(stack)
+    } else if strings.Index(s[s_pos:], "||") == 0 || strings.Index(s[s_pos:], "&&") == 0 {
+      if and_or_started {
+        return false, errors.New("Syntax error: unexpected logical operator at "+strconv.Itoa(s_pos))
+      } else {
+        stack = append(stack, s[s_pos:s_pos+2])
+        and_or_started = true
+        s_pos += 2
+      }
+    } else {
+      return false, errors.New("Syntax error: unexpected expression at "+strconv.Itoa(s_pos))
+    }
+
+  }
+
+  return resolveStack(stack)
+}
+
